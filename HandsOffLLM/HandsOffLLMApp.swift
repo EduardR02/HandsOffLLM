2af27ce9e52a023f0629c479751608f82bd95834
Eduard Rantsevich <eduard@rantsevich.net>
2025-04-08 12:43:43 +0200

    Initial Commit

---
+++ HandsOffLLM/HandsOffLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandsOffLLMApp.swift
+//  HandsOffLLM
+//
+//  Created by Eduard Rantsevich on 08.04.25.
+//
+
+import SwiftUI
+
+@main
+struct HandsOffLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HandsOffLLMTests/HandsOffLLMTests.swift
@@ -0,0 +1,17 @@
+//
+//  HandsOffLLMTests.swift
+//  HandsOffLLMTests
+//
+//  Created by Eduard Rantsevich on 08.04.25.
+//
+
+import Testing
+@testable import HandsOffLLM
+
+struct HandsOffLLMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
